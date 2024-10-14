2345e4662455cdd3eb1817616721993e95940916
MerjRam <m.ramic@students.htl-leonding.ac.at>
2024-10-14 17:36:47 +0200

    Add files via upload

--- Übung08_BesuchDerTante/BesuchDerTante.cs
+++ Übung08_BesuchDerTante/BesuchDerTante.cs
@@ -4,7 +4,61 @@
 *Merjem Ramic
 *-------------------------------------------------------------------------------------------
 *Description:
-*			Uhrzeit mit der Form hh:mm:ss
+*			Besuch der Tante/ Abfahrt ist um 10:00
 *-------------------------------------------------------------------------------------------
 */
 using System;
+
+string input;
+string input2;
+int    speed;
+double kilometers;
+double time;
+double departs = 10.00;
+double arrival;
+int    hours;
+int    minutes;
+int    seconds;
+double remainingTime;
+
+//Eingabe
+
+Console.WriteLine("Berechnung der Ankungt bei der Tante");
+Console.WriteLine("====================================");
+Console.WriteLine("");
+
+Console.Write("Entfernung in km: ");
+input 	   = Console.ReadLine();
+kilometers = double.Parse(input);
+
+Console.Write("Geschwingkeit in km/h: ");
+input2 = Console.ReadLine();
+speed  = int.Parse(input2);
+
+//Verarbeitubg
+
+time = kilometers/speed; //die Stunden der Fahrt
+
+arrival = departs + time; //die Ankuftszeit ab 10:00
+
+hours         = (int)arrival;
+remainingTime = (arrival - hours) * 60;
+minutes       = (int)remainingTime;
+seconds       = (int)((remainingTime - minutes) * 60);
+
+//Ausgabe
+
+Console.WriteLine($"Fuer die Strecke von {kilometers:F2} km benoetigen Sie {time:F4} Stunden.");
+Console.WriteLine($"Sie kommen um {hours}:{minutes:D2}:{seconds:D2} an. ");
+
+if (arrival < 12)
+{
+    Console.WriteLine("Sie bekommen ein Mittagessen! ");
+}
+else 
+{
+    Console.WriteLine("Sie bekommen nur einen Kaffe und Kuchen! ");
+}
+
+
+Console.ReadLine();
