e46c302aa4aa1142ef5df617a0284b4ca00db72c
MerjRam <m.ramic@students.htl-leonding.ac.at>
2024-10-15 17:56:30 +0200

    Update TimeAdder.cs

--- Übung07_TimeAdder/TimeAdder.cs
+++ Übung07_TimeAdder/TimeAdder.cs
@@ -8,13 +8,19 @@
 *-------------------------------------------------------------------------------------------
 */
 using System;
+
 string input;
 int minutes1;
 int minutes2;
 int hour1;
 int hour2;
-int sum1;
-int sum2;
+
+int minSum;
+int hourSum;
+
+//int totalMinSum;
+//int totalHourSum;
+
 
 //Eingabe
 Console.WriteLine("Einfacher Addierer für zwei Uhrzeiten");
@@ -25,7 +31,7 @@ input = Console.ReadLine();
 hour1 = int.Parse(input);
 
 Console.Write("Minuten 1: ");
-input = Console.ReadLine();
+input    = Console.ReadLine();
 minutes1 = int.Parse(input);
 
 Console.Write("Stunden 2: ");
@@ -33,18 +39,21 @@ input = Console.ReadLine();
 hour2 = int.Parse(input);
 
 Console.Write("Minuten 2: ");
-input = Console.ReadLine();
+input    = Console.ReadLine();
 minutes2 = int.Parse(input);
 
 //Verarbeitung
-sum1 = minutes1 + minutes2;
-sum2 = hour1 + hour2;	
 
+minSum  = minutes1 + minutes2;
+hourSum = hour1 + hour2;
+
+hourSum  += minSum / 60;
+minSum   = minSum % 60;
 
 //Ausgabe
 Console.WriteLine("");
 Console.WriteLine($"   {hour1:00}:{minutes1:00}");
 Console.WriteLine($"+  {hour2:00}:{minutes2:00}");
-Console.WriteLine($"=  {sum2:00}:{sum1:00}");
+Console.WriteLine($"=  {hourSum:00}:{minSum:00}");
 
 Console.ReadLine();
