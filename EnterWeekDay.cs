9e2c60b642705b45d6cd2628946d6c020eb586c0
MerjRam <m.ramic@students.htl-leonding.ac.at>
2024-10-18 22:28:56 +0200

    Add files via upload

---
+++ EnterWeekDay.cs
@@ -0,0 +1,62 @@
+﻿/*------------------------------------------------------------------------------------------
+*HTBLA-Leonding/Class: 3ACIF											
+*-------------------------------------------------------------------------------------------
+*Merjem Ramic
+*-------------------------------------------------------------------------------------------
+*Description:
+*			Enter Week Day
+*-------------------------------------------------------------------------------------------
+*/
+using System;
+
+string weekday;
+string secoundLetter;
+
+Console.Write("Gebe den ersten Buchstaben eines Wochentages ein:");
+weekday = Console.ReadLine();
+weekday = weekday.ToUpper(); // Groß-Kleinschreibung
+
+
+if((weekday == "M") || (weekday == "S") || (weekday == "D"))
+{
+	Console.Write("Gebe den zweiten Buchstaben des Wochentages ein:");
+	secoundLetter = Console.ReadLine().ToUpper();
+	weekday = weekday + secoundLetter;
+	
+}
+
+switch (weekday)
+{
+	case"MO":
+		Console.Write("Sie haben Montag ausgewählt.");
+		break;
+	
+	case"DI":
+		Console.Write("Sie haben Dienstag ausgewählt.");
+		break;
+	
+	case"MI":
+		Console.Write("Sie haben Mittwoch ausgewählt.");
+		break;
+	
+	case"DO":
+		Console.Write("Sie haben Donnerstag ausgewählt.");
+		break;
+	
+	case"F":
+		Console.Write("Sie haben Freitag ausgewählt.");
+		break;
+	
+	case"SA":
+		Console.Write("Sie haben Samstag ausgewählt.");
+		break;
+	
+	case"SO":
+		Console.Write("Sie haben Sonntag ausgewählt.");
+		break;
+		
+	default:
+		Console.Write("Ungültige Eingabe!");
+		break;
+	
+}
